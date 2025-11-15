21dab32de0d054db33bb8ab867cdfd66e336881f
Talvan <talvan.alves00@gmail.com]>
2025-11-15 17:23:18 -0300

    adjusting route

--- usuario_app/urls.py
+++ usuario_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name="index"),
-   path('chat/', views.chat, name="chat"),
-   path('perfil/', views.perfil, name="perfil"),
+    path('', views.index, name="index"),
+    path('chat/', views.chat, name="chat"),
+    path('perfil/', views.perfil, name="perfil"),
     path('historico/', views.historico, name="historico"),
     path('caronas/cadastrar', views.cadastrar_carona, name="cadastrar_carona"),
 ]
