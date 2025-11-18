ff4760cf7cdcf319ff83a038d360b27230e95954
FabricioS2 <fabricio.g@escolar.ifrn.edu.br>
2025-11-18 01:31:26 -0300

    Mesclando os branches

--- usuario_app/urls.py
+++ usuario_app/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name="index"),
+   path('', views.login, name="login"),
    path('chat/', views.chat, name="chat"),
    path('perfil/', views.perfil, name="perfil"),
    path('historico/', views.historico, name="historico"),
-   path('cadastrar_caronas', views.cadastrar_carona, name="cadastrar_caronas"),
+   path('cadastrar_caronas/', views.cadastrar_carona, name="cadastrar_caronas"),
    path('cadastro/', views.cadastro, name="cadastro"),
-   path('login/', views.login, name="login"),
    path('solicitar_carona/', views.solicitar_carona, name="solicitar_carona"),
 ]
--- usuario_app/views.py
+++ usuario_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'usuario_app/index.html')
 
 def chat(request):
     return render(request, 'usuario_app/chat.html')
